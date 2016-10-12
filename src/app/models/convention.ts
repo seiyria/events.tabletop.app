02bc842e42d480e34176da585afbd562f1a8665f
seiyria <kyle@seiyria.com>
2016-10-11 23:09:22 -0500

    fix news date

--- src/app/models/convention.ts
+++ src/app/models/convention.ts
@@ -1,4 +1,6 @@
 
+import _ from 'lodash';
+
 export const RELATED_MODELS = ['geolocation', 'venue', 'updates', 'badges'];
 
 export class Convention {
@@ -12,4 +14,8 @@ export class Convention {
 export const fixConvention = (convention) => {
   convention.start_date = new Date(convention.start_date.split('-').join('/'));
   convention.end_date = new Date(convention.end_date.split('-').join('/'));
+
+  _.each(convention._news, news => {
+    news.date_sent = new Date(news.date_sent.split('-').join('/'));
+  });
 };
