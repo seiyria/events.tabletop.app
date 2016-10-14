2ae7e8a76b130f619b7262662905ba2db0dfcae9
seiyria <kyle@seiyria.com>
2016-10-14 18:53:45 -0500

    closes #29, closes #31

--- src/app/models/event.ts
+++ src/app/models/event.ts
@@ -11,6 +11,8 @@ export class Event {
     public available_count: string,
     public max_tickets: string,
     public room_name: string,
+    public has_more_info: boolean,
+    public more_info_uri: string,
 
     public type: any,
     public user: any
@@ -19,4 +21,5 @@ export class Event {
 
 export const fixEvent = (event: Event) => {
   event.start_date = new Date(event.start_date.split('-').join('/'));
+  event.has_more_info = event.more_info_uri.length > 10;
 };
